99daa478bb192ceb6d2bb162e4d77b30d29f668c
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-03-30 12:08:23 +0300

    todo8

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -370,6 +370,13 @@ export default function App() {
   //     setStudent(data);
   //   });
 
+   useEffect(() => {
+  fetch('http://localhost:3000/student')
+    .then((res) => res.json())
+    .then((data) => {
+      setStudent(data);
+    });
+   }, []);
   return (
     <main className="app-shell">
       <section className="hero-box">
