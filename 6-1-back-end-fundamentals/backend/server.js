ead519199d7b31c112cb59505a133f1a006e7c6e
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-03-30 11:58:19 +0300

    todo2

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -2,10 +2,10 @@
 
 import express from 'express';
 
-
 // TODO 2: Create the Express app and store it in a variable named app
 
-
+const app = express();
+app.use(cors());
 
 // TODO 3: Allow React to access the server
 
