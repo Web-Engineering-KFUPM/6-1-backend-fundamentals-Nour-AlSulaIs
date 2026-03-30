b569c687ce424dcab23c45a6d1fed7f092fdcc9c
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-03-30 12:01:37 +0300

    todo4and5

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -5,14 +5,16 @@ import express from 'express';
 // TODO 2: Create the Express app and store it in a variable named app
 
 const app = express();
-app.use(cors());
 
 // TODO 3: Allow React to access the server
 
 import cors from 'cors';
+app.use(cors());
 
 // TODO 5: Create the home route "/"
-
+app.get('/', (req, res) => {
+  res.send('Hello from the back-end server');
+});
 
 
 // TODO 6: Create the "/about" route
